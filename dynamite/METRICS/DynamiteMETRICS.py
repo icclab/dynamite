949cd82efebdc02e0dc526169ad72f2f95d54a44
brnr <brnr@zhaw.ch>
2015-06-23 17:32:21 +0200

    added METRICS component

--- dynamite/METRICS/DynamiteMETRICS.py
+++ dynamite/METRICS/DynamiteMETRICS.py
@@ -1,9 +1,185 @@
 __author__ = 'brnr'
 
+import time
+import json
+
 from multiprocessing import Process
+
 from dynamite.GENERAL.DynamiteExceptions import IllegalArgumentError
-from dynamite.INIT.DynamiteConfig import DynamiteConfig
 from dynamite.GENERAL import ETCDCTL
+from dynamite.INIT.DynamiteConfig import DynamiteConfig
+from dynamite.METRICS.EtcdServiceMetricInformation import EtcdServiceMetricInformation
+from dynamite.ENGINE.MetricsMessage import MetricsMessage
+from dynamite.ENGINE.MetricValue import MetricValue
+
+class DynamiteMETRICS(Process):
+
+    etcd_endpoint = None
+    etcdctl = None
+    dynamite_config = None
+    scaling_engine_metrics_communication_queue = None
+    etcd_service_metric_information_instances = []
+    running = False
+
+    # service-name --> [metric-names]
+    # this dictionary is used to keep track of service and metrics and helps to ensure there are no double entries
+    service_metrics_dictionary = {}
+
+    def _init_etcdctl(self, etcd_endpoint):
+        etcdctl = ETCDCTL.create_etcdctl(etcd_endpoint)
+
+        if etcdctl is not None:
+            return etcdctl
+        else:
+            return None
+
+    def _etcd_get_uuids(self, metrics_base_path):
+        r = self.etcdctl.read(metrics_base_path, recursive=True, sorted=True)
+
+        uuids = []
+
+        for child in r.children:
+            uuid = child.key.split("/")[3]
+
+            if uuid not in uuids:
+                uuids.append(uuid)
+
+        return uuids
+
+    def _update_uuids(self):
+        for instance in self.etcd_service_metric_information_instances:
+            if not instance.is_aggregated:
+                uuids = self._etcd_get_uuids(instance.metric_base_path)
+                instance.uuids = uuids
+
+    def _create_etcd_service_metric_information_instances(self, dynamite_config):
+        metrics_path = dynamite_config.ETCD.metrics_base_path
+
+        for policy_name, policy in dynamite_config.ScalingPolicy.__dict__.items():
+
+            service_name = policy.service
+            metric_name = policy.metric
+
+            double_entry = False
+
+            if service_name in self.service_metrics_dictionary:
+                metrics = self.service_metrics_dictionary[service_name]
+
+                if metric_name in metrics:
+                    double_entry = True
+                else:
+                    self.service_metrics_dictionary[service_name].append(metric_name)
+            else:
+                self.service_metrics_dictionary[service_name] = [metric_name]
+
+            if not double_entry:
+                metrics_base_path = metrics_path + "/" + service_name
+
+                is_aggregated = policy.metric_aggregated
+
+                uuids = []
+
+                # if metrics are not aggregated get the different uuids resp. instances
+                if not is_aggregated:
+                    uuids = self._etcd_get_uuids(metrics_base_path)
+
+                # create metric path per EtcdServiceMetricInformation instance
+                etcd_metric_paths = []
+
+                if is_aggregated and service_name == "loadbalancer":
+                    metric_and_detail = metric_name.split(".", 1)
+                    metric = metric_and_detail[0]
+                    detail = metric_and_detail[1]   # detail from json response
+
+                    etcd_metric_path = metrics_base_path
+                    etcd_metric_path += "/" + metric
+
+                    etcd_metric_paths.append(etcd_metric_path)
+                else:
+                    metric = metric_name
+                    etcd_metric_base_path = metrics_base_path
+
+                    for uuid in uuids:
+                        etcd_metric_path = etcd_metric_base_path + "/" + uuid + "/" + metric
+                        etcd_metric_paths.append(etcd_metric_path)
+
+                etcd_service_metric_information = EtcdServiceMetricInformation(service_name,
+                                                                               metric_name,
+                                                                               metrics_base_path,
+                                                                               is_aggregated,
+                                                                               uuids,
+                                                                               etcd_metric_paths)
+
+                self.etcd_service_metric_information_instances.append(etcd_service_metric_information)
+
+    def run(self):
+
+        self.dynamite_config = DynamiteConfig(etcd_endpoint=self.etcd_endpoint)
+        self.etcdctl = self._init_etcdctl(self.etcd_endpoint)
+        self._create_etcd_service_metric_information_instances(self.dynamite_config)
+
+        while self.running:
+
+            for instance in self.etcd_service_metric_information_instances:
+
+                for etcd_metric_path in instance.etcd_metric_paths:
+
+                    r = self.etcdctl.read(etcd_metric_path, recursive=True, sorted=True)
+
+                    metric_values = []
+                    uuid = None
+                    value = None
+
+                    # one child is represents one metric (key and value)
+                    for child in r.children:
+
+                        if child.value is not None:
+                            timestamp = child.key.split("/")[-1]
+                            value = child.value
+
+                            if instance.service_name == "loadbalancer":
+                                # metric name will be like: response_time.time_backend_response.p95
+                                metric_from_json = instance.metric_name.split(".", 1)[-1]
+                                response_json = json.loads(value)
+                                value = response_json[metric_from_json]
+                            else:
+                                value = float(value)
+                                uuid = child.key.split("/")[3]
+
+                            metric_value = MetricValue(timestamp, value)
+                            metric_values.append(metric_value)
+
+                            self.etcdctl.delete(child.key)
+
+                    if len(metric_values) > 0:
+                        metrics_message = MetricsMessage(instance.service_name,
+                                                         uuid,
+                                                         metric_values,
+                                                         instance.metric_name)
+
+                        print(metrics_message)
+                        self.scaling_engine_metrics_communication_queue.put(metrics_message)
+                    else:
+                        print("nosing to see")
+
+            time.sleep(1)
+
+    def __init__(self, etcd_endpoint, scaling_engine_metrics_communication_queue):
+        super(DynamiteMETRICS, self).__init__()
+
+        if etcd_endpoint is not None and isinstance(etcd_endpoint, str):
+            self.etcd_endpoint = etcd_endpoint
+
+        else:
+            raise IllegalArgumentError("Error: argument <etcd_endpoint> needs to be of type <str>")
+
+        self.scaling_engine_metrics_communication_queue = scaling_engine_metrics_communication_queue
+
+        self.running = True
+
+if __name__ == '__main__':
+    dynamite_metrics = DynamiteMETRICS("127.0.0.1:4001", None)
+    dynamite_metrics.start()
 
 
 # the folder 'response_time' (could) contain a bunch of entries
@@ -37,62 +213,3 @@ from dynamite.GENERAL import ETCDCTL
 # the folder 'cpu_user' (could) contain a bunch of entries
 # /metrics/webserver/e2213560-5b2f-4d2d-9d85-2cd878700259/metrics/cpu/cpu_user/2015-06-22T14:59:00.915Z
 # value
-
-class DynamiteMETRICS(Process):
-
-    etcd_endpoint = None
-    etcdctl = None
-    dynamite_config = None
-    scaling_engine_metrics_communication_queue = None
-    running = False
-
-    def _do_stuff_with_config(self, dynamite_config):
-        print(dynamite_config)
-
-    def _init_etcdctl(self, arg_etcd_endpoint):
-        etcdctl = ETCDCTL.create_etcdctl(arg_etcd_endpoint)
-
-        if etcdctl is not None:
-            return etcdctl
-        else:
-            return None
-
-    def run(self):
-
-        self.dynamite_config = DynamiteConfig(etcd_endpoint=self.etcd_endpoint)
-        self.etcdctl = self._init_etcdctl(self.etcd_endpoint)
-
-        # build path to get the needed application metrics
-        application_metric_paths = []
-
-        # /metrics/loadbalancer/metrics/response_time/2015-06-22T14:54:12.319Z
-        # /metrics/webserver/e2213560-5b2f-4d2d-9d85-2cd878700259/metrics/cpu/cpu_user/2015-06-22T14:59:00.915Z
-
-        self._do_stuff_with_config(self.dynamite_config)
-
-        while self.running:
-            self.running = False
-            # get metrics from etcd
-            # iterate through application_metric_paths list
-
-            # create MetricsMessage and MetricValue
-
-            # send MetricsMessage to scaling_engine_metris_communication queue
-            # self.xxxQueue.put(product)
-
-    def __init__(self, etcd_endpoint, scaling_engine_metrics_communication_queue):
-        super(DynamiteMETRICS, self).__init__()
-
-        if etcd_endpoint is not None and isinstance(etcd_endpoint, str):
-            self.etcd_endpoint = etcd_endpoint
-
-        else:
-            raise IllegalArgumentError("Error: argument <etcd_endpoint> needs to be of type <str>")
-
-        self.scaling_engine_metrics_communication_queue = scaling_engine_metrics_communication_queue
-
-        self.running = True
-
-if __name__ == '__main__':
-    dynamite_metrics = DynamiteMETRICS("127.0.0.1:4001", None)
-    dynamite_metrics.start()
--- dynamite/METRICS/EtcdServiceMetricInformation.py
+++ dynamite/METRICS/EtcdServiceMetricInformation.py
@@ -1,7 +1,18 @@
 __author__ = 'brnr'
 
 class EtcdServiceMetricInformation(object):
+
+    service_name = None         # from ScalingPolicy.<policy_name>
+    metric_name = []           # from ScalingPolicy.metric
     metric_base_path = None     # from ETCD.metrics_base_path + service_name
     is_aggregated = None
-    uuids = []                  # if
-    service_name                # from ScalingPolicy.<policy_name>
+    uuids = []                  # if not aggregated saves list of uuids
+    etcd_metric_paths = None
+
+    def __init__(self, service_name, metric_name, metric_base_path, is_aggregated, uuids, etcd_metric_paths):
+        self.service_name = service_name
+        self.metric_name = metric_name
+        self.metric_base_path = metric_base_path
+        self.is_aggregated = is_aggregated
+        self.uuids = uuids
+        self.etcd_metric_paths = etcd_metric_paths
